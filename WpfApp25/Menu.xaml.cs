e549a3fdb4806f7d4fe6cc44ea417fc99ef883b8
CaLink <caplink777@gmail.com>
2021-06-07 13:09:28 +1000

    Добавьте файлы проекта.

---
+++ WpfApp25/Menu.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp25
+{
+    /// <summary>
+    /// Логика взаимодействия для Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        ImageBrush myGridSkin = new ImageBrush();
+
+        public Menu()
+        {
+            InitializeComponent();
+            mygrid.Background = myGridSkin;
+            myGridSkin.ImageSource = new BitmapImage(new Uri("Images/SpaceForGame.png", UriKind.Relative));
+            mygrid.Focus();
+        }
+
+        private void baton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayWithFriend playWithFriend = new PlayWithFriend();
+            playWithFriend.Show();
+            Close();
+        }
+
+        private void SimpleGame_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void BossGame_Click(object sender, RoutedEventArgs e)
+        {
+            Boss boss = new Boss();
+            boss.Show();
+            Close();
+        }
+    }
+}
